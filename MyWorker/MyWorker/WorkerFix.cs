065df2810930a32b011d4ea075b173189ab702c9
EkaterinaSkachkova <easkachkova@mail.ru>
2019-02-15 16:47:33 +0300

    Департаменты и сотрудники вроде бы использовала связывание, ListView, ObservableCollection, INotifyPropertyChanged, но есть проблемы, тяжеловато у меня идет.
    при удалении департемента должны удалиться и сотрудники - загвоздка, коллекция сотрудников остается той же
    с удалением сотрудников при клике на кнопку "Удалить" также ничего не происходит
    при открытии дочерней формы, название департамента выводится столбцом

---
+++ MyWorker/MyWorker/WorkerFix.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWorker
+{
+    class WorkerFix:BasicWorker, IComparable<WorkerFix>
+    {
+        public double Salary { get; set; }
+
+        public WorkerFix(string Name, int Age, double Salary) : base(Name, Age)
+        {
+            this.Salary = AverageSalary(Salary);
+        }
+
+        public override double AverageSalary(double a)
+        {
+            double Salary = a;
+            return Salary;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"{this.Salary} ";
+        }
+
+        public int CompareTo(WorkerFix obj)
+        {
+            return obj.Salary > this.Salary ? -1 : 1;
+        }
+    }
+}
